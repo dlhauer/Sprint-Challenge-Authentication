011d91572d3cd1617588202bce8028ac3ee7d6cb
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-31 09:17:58 -0800

    Authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,21 @@
-/* 
-  complete the middleware code to check if the user is logged in
-  before granting access to the next middleware/route handler
-*/
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, jwtSecret, (error, decodedToken) => {
+      if (error) {
+        console.log(error);
+        res.status(401).json({
+          message: 'You tampered with that token. Police have been dispatched to your home to detain you.'
+        })
+      } else {
+        req.user = decodedToken.user;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
