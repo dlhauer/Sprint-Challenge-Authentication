14bbacf566d88b4c3ad6c3ce5acc348cb1f82295
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-31 09:02:00 -0800

    Add register and login endpoints

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,46 @@
 const router = require('express').Router();
+const bc = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config/secrets');
+const Users = require('./auth-model');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bc.hashSync(user.password); 
+  user.password = hash;
+  Users.add(user)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: 'Error saving new user.'
+      });
+    });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bc.compareSync(password, user.password)) {
+        const token = signToken(user);
+        res.status(200).json({token: token})
+      } else {
+        res.status(401).json({ message: 'Invalid kridencials!'})
+      }
+    })
 });
+
+function signToken(user) {
+  const payload = {
+    user
+  };
+  const options = {
+    expiresIn: '1h'
+  };
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
